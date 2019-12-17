eeb52fa373a7559204efeedc29fec8e2e0e5dd97
Tomas Lebl <tomlebl44@gmail.com>
2019-12-17 22:37:05 +0000

    Day 8 finished

--- 8/santa8-2.js
+++ 8/santa8-2.js
@@ -5,24 +5,30 @@ const inputStr = fs.readFileSync('input.txt', { encoding: 'utf-8' })
 const layersArr = []
 
 for (let i = 0; i < inputStr.length; i += 25 * 6) {
-   layersArr.push(
-      inputStr
-         .slice(i, i + 25 * 6)
-         .split('')
-         .map(input => Number(input))
-   )
+	layersArr.push(
+		inputStr
+			.slice(i, i + 25 * 6)
+			.split('')
+			.map(input => Number(input))
+	)
 }
 
 const pixelsArr = []
 
 for (let i = 0; i < 150; i++) {
-   const pixel = []
-   for (let j = 0; j < layersArr.length; j++) {
-      pixel.push(layersArr[j][i])
-   }
-   pixelsArr.push(pixel)
+	const pixel = []
+	for (let j = 0; j < layersArr.length; j++) {
+		pixel.push(layersArr[j][i])
+	}
+	pixelsArr.push(pixel)
 }
 
 const pixelsArr2 = pixelsArr.map(pixel => pixel.find(e => e != 2))
 
-console.log(pixelsArr2)
+const display = []
+
+for (let i = 0; i < 6; i++) {
+	display.push(pixelsArr2.splice(0, 25).join(''))
+}
+
+console.log(display)
