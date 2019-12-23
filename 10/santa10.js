6e8e9b5e3fcdb8318378a546c5a723ff31608c15
Tomas Lebl <tomlebl44@gmail.com>
2019-12-23 23:40:46 +0000

    Day 10 - part 2 finished

--- 10/santa10.js
+++ 10/santa10.js
@@ -1,6 +1,6 @@
 const fs = require('fs')
 
-const spaceMatrix = fs.readFileSync('test1.txt', { encoding: 'utf-8' }).split('\n')
+const spaceMatrix = fs.readFileSync('input.txt', { encoding: 'utf-8' }).split('\n')
 
 const asteroidArr = []
 
@@ -12,6 +12,8 @@ spaceMatrix.forEach((row, index) => {
 	}
 })
 
+// PART 1
+
 const findBase = asteroidArr => {
 	let base = {
 		x: 0,
@@ -20,23 +22,19 @@ const findBase = asteroidArr => {
 		index: 0
 	}
 	asteroidArr.forEach((asteroid, index) => {
-		let slopeSet1 = new Set()
-		let slopeSet2 = new Set()
+		let angleSet = new Set()
 
 		for (let i = 0; i < asteroidArr.length; i++) {
 			asteroidArr[i].index = i
-			const slope = (asteroidArr[i].x - asteroid.x) / (asteroidArr[i].y - asteroid.y)
 
-			if (i < index) {
-				slopeSet1.add(slope)
-			} else if (i > index) {
-				slopeSet2.add(slope)
+			if (i != index) {
+				angleSet.add(Math.atan2(asteroidArr[i].x - asteroid.x, asteroidArr[i].y - asteroid.y))
 			}
 		}
-		let asteroidsDetected = slopeSet1.size + slopeSet2.size
-		if (asteroidsDetected > base.canDetect) {
+
+		if (angleSet.size > base.canDetect) {
 			Object.assign(base, asteroid)
-			base.canDetect = asteroidsDetected
+			base.canDetect = angleSet.size
 		}
 	})
 	return base
@@ -45,20 +43,48 @@ const findBase = asteroidArr => {
 const base = findBase(asteroidArr)
 console.log(base)
 
+//PART 2
+
 asteroidArr.forEach(asteroid => {
 	asteroid.distance = Math.sqrt(Math.pow(base.x - asteroid.x, 2) + Math.pow(base.y - asteroid.y, 2))
-	asteroid.angle = (Math.atan((base.x - asteroid.x) / Math.abs(base.y - asteroid.y)) * 180) / Math.PI
+	asteroid.angle = (Math.atan2(asteroid.x - base.x, asteroid.y - base.y) * 180) / Math.PI
 })
 
-asteroidArr.sort((a, b) => a.angle - b.angle)
+asteroidArr.sort((a, b) => b.angle - a.angle)
 
 const asteroidMap = new Map()
 asteroidArr.forEach(asteroid => {
-	if (asteroidMap.has(asteroid.angle)) {
-		asteroidMap.get(asteroid.angle).push(asteroid)
-	} else {
-		asteroidMap.set(asteroid.angle, [asteroid])
+	if (asteroid.index != base.index) {
+		if (asteroidMap.has(asteroid.angle)) {
+			asteroidMap.get(asteroid.angle).push(asteroid)
+		} else {
+			asteroidMap.set(asteroid.angle, [asteroid])
+		}
 	}
 })
 
-console.log(asteroidMap.size)
+const anglesAsterArr = Array.from(asteroidMap.values())
+
+anglesAsterArr.map(angle => {
+	angle.sort((a, b) => a.distance - b.distance)
+})
+
+const targetArr = []
+let angleCount = 0
+let distanceCount = 0
+
+console.log(anglesAsterArr.length)
+
+do {
+	// console.log(angleCount, distanceCount)
+	if (anglesAsterArr[angleCount][distanceCount]) {
+		targetArr.push(anglesAsterArr[angleCount][distanceCount])
+	}
+	angleCount++
+	if (angleCount === anglesAsterArr.length) {
+		distanceCount++
+		angleCount = 0
+	}
+} while (targetArr.length < asteroidArr.length - 1)
+
+console.log(targetArr[199].x * 100 + targetArr[199].y)
